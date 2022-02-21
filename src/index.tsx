8191c3961c020ceaa8cdf20f7cd9776c8692a78d
Nicholas <nicholas.newman1@hotmail.com>
2022-02-21 10:14:04 -0500

    setup redux firebase auth provider

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { ReactReduxFirebaseAuthProvider } from 'react-redux-firebase-auth';
 import App from './App';
+import reactReduxFirebaseAuthConfig from './reactReduxFirebaseAuthConfig';
+import { app } from './services/firebase';
+import store from './store';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <ReactReduxFirebaseAuthProvider
+        store={store}
+        app={app}
+        config={reactReduxFirebaseAuthConfig}
+      >
+        <App />
+      </ReactReduxFirebaseAuthProvider>
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
