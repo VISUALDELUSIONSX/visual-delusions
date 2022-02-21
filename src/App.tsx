f6869e86ce608e836f4077f8108189a5ed48b785
Nicholas <nicholas.newman1@hotmail.com>
2022-02-21 10:13:05 -0500

    added firestore text editor to about page

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,7 @@ import { theme } from './theme';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Admin from './pages/Admin';
 import Login from './pages/Login';
-import store from './store';
-import { ReactReduxFirebaseAuthProvider } from 'react-redux-firebase-auth';
 import { app } from './services/firebase';
-import reactReduxFirebaseAuthConfig from './reactReduxFirebaseAuthConfig';
-import { Provider } from 'react-redux';
 import PrivateRoute from './components/PrivateRoute';
 import Dialogs from './containers/Dialogs';
 import Shop from './pages/Shop';
@@ -18,38 +14,41 @@ import { FirestoreTextEditorProvider } from 'firestore-text-editor';
 import About from './pages/About';
 import FAQ from './pages/FAQ';
 import Contact from './pages/Contact';
+import Spinner from './components/Spinner';
+import { useAppSelector } from './hooks/useAppSelector';
 
 function App() {
+  const isAdmin = useAppSelector((state) => state.auth.user?.isAdmin);
+
   return (
     <Router>
-      <Provider store={store}>
-        <FirestoreTextEditorProvider app={app}>
-          <ReactReduxFirebaseAuthProvider
-            store={store}
-            app={app}
-            config={reactReduxFirebaseAuthConfig}
-          >
-            <ThemeProvider theme={theme}>
-              <CssBaseline />
-              <Dialogs />
-              <Header />
-              <main>
-                <ScrollToTop />
-                <Switch>
-                  <Route exact path='/' component={Home} />
-                  <Route exact path='/login' component={Login} />
-                  <Route exact path='/about' component={About} />
-                  <Route exact path='/faq' component={FAQ} />
-                  <Route exact path='/contact' component={Contact} />
-                  <Route exact path='/shop' component={Shop} />
-                  <Route exact path='/shop/:category' component={Shop} />
-                  <PrivateRoute exact path='/admin' component={Admin} />
-                </Switch>
-              </main>
-            </ThemeProvider>
-          </ReactReduxFirebaseAuthProvider>
-        </FirestoreTextEditorProvider>
-      </Provider>
+      <FirestoreTextEditorProvider
+        app={app}
+        editIconStyle={{ color: '#aaa' }}
+        saveIconStyle={{ color: '#aaa' }}
+        cancelIconStyle={{ color: '#aaa' }}
+        loader={<Spinner />}
+        isEditable={!!isAdmin}
+      >
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Dialogs />
+          <Header />
+          <main>
+            <ScrollToTop />
+            <Switch>
+              <Route exact path='/' component={Home} />
+              <Route exact path='/login' component={Login} />
+              <Route exact path='/about' component={About} />
+              <Route exact path='/faq' component={FAQ} />
+              <Route exact path='/contact' component={Contact} />
+              <Route exact path='/shop' component={Shop} />
+              <Route exact path='/shop/:category' component={Shop} />
+              <PrivateRoute exact path='/admin' component={Admin} />
+            </Switch>
+          </main>
+        </ThemeProvider>
+      </FirestoreTextEditorProvider>
     </Router>
   );
 }
--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,7 +1,30 @@
-import React from 'react';
+import { alpha, Card, Container } from '@material-ui/core';
+import { FirestoreTextEditor } from 'firestore-text-editor';
+import NeonTypography from '../components/NeonTypography';
+import { theme } from '../theme';
 
 const About = () => {
-  return <div></div>;
+  return (
+    <Container maxWidth='md' style={{ marginTop: '3rem' }}>
+      <Card
+        style={{
+          boxShadow: `2px 2px 20px ${alpha(theme.palette.info.main, 0.4)}`,
+          padding: '2rem',
+        }}
+      >
+        <NeonTypography
+          gutterBottom
+          component='h1'
+          align='center'
+          variant='h3'
+          extraColor='info'
+        >
+          ABOUT
+        </NeonTypography>
+        <FirestoreTextEditor path='pages/about' />
+      </Card>
+    </Container>
+  );
 };
 
 export default About;
