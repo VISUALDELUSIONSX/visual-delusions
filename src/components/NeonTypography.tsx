4bea160522b93d47b03b412b3092c41be1a904da
Nicholas <nicholas.newman1@hotmail.com>
2022-04-21 17:11:29 -0400

    added animation prop to NeonTypography

--- src/components/NeonTypography.tsx
+++ src/components/NeonTypography.tsx
@@ -1,13 +1,16 @@
 import { Typography, TypographyProps } from '@material-ui/core';
 import React from 'react';
+import { useInView } from 'react-intersection-observer';
+import useAnimation, { AnimationType } from '../hooks/useAnimation';
 import { theme } from '../theme';
 
 export default function NeonTypography<C extends React.ElementType>(
   props: TypographyProps<C, { component?: C }> & {
     extraColor?: 'info';
+    animation?: AnimationType;
   }
 ) {
-  const { extraColor, ...rest } = props;
+  const { extraColor, animation, ...rest } = props;
   const color = extraColor
     ? theme.palette[extraColor].main
     : !props.color || props.color === 'inherit' || props.color === 'initial'
@@ -24,10 +27,18 @@ export default function NeonTypography<C extends React.ElementType>(
       : theme.typography[props.variant].fontSize;
 
   const shadowSize = parseInt(size + 'rem' || '1rem') / 5 + 'rem';
+
+  const [ref, , entry] = useInView();
+  const isIntersecting = entry?.isIntersecting;
+  const animationStyles = useAnimation(animation || 'fadeIn', isIntersecting, {
+    transition: '300ms ease-in-out',
+  });
   return (
     <Typography
       {...rest}
+      ref={ref}
       style={{
+        ...(animation ? animationStyles : {}),
         ...props.style,
         textShadow: `0 0 calc(${shadowSize} / 4) #fff, 0 0 calc(${shadowSize} / 4) #fff, 0 0 ${shadowSize} ${color}, 0 0 ${shadowSize} ${color}, 0 0 calc(${shadowSize} * 1.5) ${color}, 0 0 ${shadowSize} ${color}, 0 0 ${shadowSize} ${color}`,
         letterSpacing: '1.5px',
