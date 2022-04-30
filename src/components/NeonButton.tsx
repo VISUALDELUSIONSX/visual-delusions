4c7ec710790d8f70a5cdfd3de54c58e0012cd04b
Nicholas <nicholas.newman1@hotmail.com>
2022-04-30 06:51:25 -0400

    added info color support to NeonButton

--- src/components/NeonButton.tsx
+++ src/components/NeonButton.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 import { theme } from '../theme';
 
 interface Props extends ButtonProps {
-  extraColor?: 'textSecondary' | 'error';
+  extraColor?: 'textSecondary' | 'error' | 'info';
 }
 
 const NeonButton: React.FC<Props> = (props) => {
@@ -12,6 +12,8 @@ const NeonButton: React.FC<Props> = (props) => {
       ? theme.palette.text.secondary
       : props.extraColor === 'error'
       ? theme.palette.error.main
+      : props.extraColor === 'info'
+      ? theme.palette.info.main
       : !props.color || props.color === 'inherit' || props.color === 'default'
       ? 'white'
       : theme.palette[props.color].main;
@@ -22,9 +24,11 @@ const NeonButton: React.FC<Props> = (props) => {
     <Button
       style={{
         ...props.style,
-        boxShadow: `0px 0px 10px 5px ${color}`,
+        boxShadow: props.disabled ? undefined : `0px 0px 10px 5px ${color}`,
         color: 'white',
-        textShadow: `0 0 calc(${shadowSize} / 4) #fff,  0 0 ${shadowSize} ${color}, 0 0 ${shadowSize} ${color}, 0 0 calc(${shadowSize} * 1.5) ${color}, 0 0 ${shadowSize} ${color}, 0 0 ${shadowSize} ${color}`,
+        textShadow: props.disabled
+          ? undefined
+          : `0 0 calc(${shadowSize} / 4) #fff,  0 0 ${shadowSize} ${color}, 0 0 ${shadowSize} ${color}, 0 0 calc(${shadowSize} * 1.5) ${color}, 0 0 ${shadowSize} ${color}, 0 0 ${shadowSize} ${color}`,
       }}
       {...props}
     />
