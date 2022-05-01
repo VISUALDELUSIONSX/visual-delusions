6ad01373ba1aac19268922ebd0de66411df02070
Nicholas <nicholas.newman1@hotmail.com>
2022-05-01 12:32:11 -0400

    subtotal bug fix

--- src/utils/getSubtotal.ts
+++ src/utils/getSubtotal.ts
@@ -1,6 +1,9 @@
 import { CartItem } from '../types/client';
 
 const getSubtotal = (cartItems: CartItem[]) =>
-  cartItems.reduce((acc, cur) => acc + cur.quantity * cur.price, 0);
+  cartItems.reduce(
+    (acc, cur) => acc + (cur.savedForLater ? 0 : cur.quantity * cur.price),
+    0
+  );
 
 export default getSubtotal;
