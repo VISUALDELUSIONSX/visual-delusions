582d38063d0c23187e772ab0f0d279f849a86489
Nicholas <nicholas.newman1@hotmail.com>
2022-05-08 06:45:53 -0400

    added emulator check back

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -26,10 +26,10 @@ export type WhereFilterOp = firebase.firestore.WhereFilterOp;
 export const callableContactSubmission =
   functions.httpsCallable('contactSubmission');
 
-// if (process.env.NODE_ENV === 'development') {
-//   auth.useEmulator('http://localhost:9099');
-//   db.useEmulator('localhost', 8080);
-//   functions.useEmulator('localhost', 5001);
-//   storage.useEmulator('localhost', 9199);
-// }
+if (process.env.NODE_ENV === 'development') {
+  auth.useEmulator('http://localhost:9099');
+  db.useEmulator('localhost', 8080);
+  functions.useEmulator('localhost', 5001);
+  storage.useEmulator('localhost', 9199);
+}
 
