c18dc662f8133902ea5ecabf7a5ea21b62c0c0b7
Nicholas <nicholas.newman1@hotmail.com>
2022-05-08 11:23:34 -0400

    added animations to NeonCard

--- src/components/NeonCard.tsx
+++ src/components/NeonCard.tsx
@@ -1,19 +1,32 @@
 import { alpha, Card, CardProps } from '@material-ui/core';
+import useAnimation, { AnimationType } from '../hooks/useAnimation';
 import React from 'react';
+import { useInView } from 'react-intersection-observer';
 
-const NeonCard: React.FC<CardProps & { shadowColor?: string }> =
-  React.forwardRef((props, ref) => {
-    const { shadowColor, ...rest } = props;
-    return (
+const NeonCard: React.FC<
+  CardProps & { shadowColor?: string; animation?: AnimationType }
+> = React.forwardRef((props, forwardRef) => {
+  const { animation, shadowColor, ...rest } = props;
+
+  const [ref, , entry] = useInView();
+  const isIntersecting = entry?.isIntersecting;
+  const animationStyles = useAnimation(animation || 'fadeIn', isIntersecting, {
+    transition: '300ms ease-in-out',
+  });
+
+  return (
+    <div ref={ref}>
       <Card
         {...rest}
-        ref={ref}
+        ref={forwardRef}
         style={{
           boxShadow: `2px 2px 20px ${alpha(shadowColor || '#fff', 0.4)}`,
+          ...animationStyles,
           ...props.style,
         }}
       />
-    );
-  });
+    </div>
+  );
+});
 
 export default NeonCard;
