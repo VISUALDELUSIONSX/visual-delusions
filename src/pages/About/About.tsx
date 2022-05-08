f2a6ccb04a3c74eecb861dc2672621496b6bf53e
Nicholas <nicholas.newman1@hotmail.com>
2022-05-08 11:25:28 -0400

    about page

---
+++ src/pages/About/About.tsx
@@ -0,0 +1,61 @@
+import { Container, Grid } from '@material-ui/core';
+import NeonTypography from '../../components/NeonTypography';
+import { theme } from '../../theme';
+import Section from './Section';
+
+const About = () => {
+  return (
+    <Container maxWidth='lg' style={{ marginTop: '3rem' }}>
+      <NeonTypography
+        animation='fadeIn'
+        component='h1'
+        variant='h2'
+        shadowQuotient={4}
+        color={theme.palette.warning.dark}
+        align='center'
+        style={{
+          marginBottom: '4rem',
+          marginTop: '4rem',
+          fontWeight: 'bold',
+        }}
+      >
+        ABOUT US
+      </NeonTypography>
+
+      <Grid container direction='column' spacing={8}>
+        <Grid item>
+          <Section
+            color={theme.palette.info.main}
+            path='pages/about'
+            imageField='ourPurposeSrc'
+            textField='ourPurpose'
+            title='Our Purpose'
+          />
+        </Grid>
+
+        <Grid item>
+          <Section
+            reverse
+            color={theme.palette.error.main}
+            path='pages/about'
+            imageField='theArtistSrc'
+            textField='theArtist'
+            title='The Artist'
+          />
+        </Grid>
+
+        <Grid item>
+          <Section
+            color={theme.palette.secondary.main}
+            path='pages/about'
+            imageField='howItStartedSrc'
+            textField='howItStarted'
+            title='How It Started'
+          />
+        </Grid>
+      </Grid>
+    </Container>
+  );
+};
+
+export default About;
---
+++ src/pages/About/Section.tsx
@@ -0,0 +1,81 @@
+import { Grid } from '@material-ui/core';
+import { FirestoreTextEditor } from 'firestore-text-editor';
+import NeonCard from '../../components/NeonCard';
+import NeonTypography from '../../components/NeonTypography';
+import ImageDropzoneAndDisplayContainer from '../../containers/ImageDropzoneAndDisplayContainer';
+
+interface Props {
+  title: string;
+  path: string;
+  imageField: string;
+  textField: string;
+  reverse?: boolean;
+  color: string;
+}
+
+const Section: React.FC<Props> = ({
+  title,
+  path,
+  imageField,
+  textField,
+  color,
+  reverse,
+}) => {
+  const smallSection = (
+    <Grid
+      item
+      xs={12}
+      sm={5}
+      md={4}
+      style={{ height: 400, width: 400, alignSelf: 'center' }}
+    >
+      <ImageDropzoneAndDisplayContainer
+        storagePath={path}
+        docPath={path}
+        field={imageField}
+      />
+    </Grid>
+  );
+
+  const largeSection = (
+    <Grid item xs={12} sm={7} md={8}>
+      <NeonTypography color={color} component='h2' variant='h4' gutterBottom>
+        {title}
+      </NeonTypography>
+      <FirestoreTextEditor
+        editorStyle={{ lineHeight: '200%' }}
+        path={path}
+        field={textField}
+      />
+    </Grid>
+  );
+
+  return (
+    <section>
+      <NeonCard shadowColor={color}>
+        <div style={{ padding: '2rem' }}>
+          <Grid
+            container
+            direction='row'
+            spacing={4}
+            wrap={reverse ? 'wrap' : 'wrap-reverse'}
+          >
+            {reverse ? (
+              <>
+                {largeSection}
+                {smallSection}
+              </>
+            ) : (
+              <>
+                {smallSection}
+                {largeSection}
+              </>
+            )}
+          </Grid>
+        </div>
+      </NeonCard>
+    </section>
+  );
+};
+
+export default Section;
